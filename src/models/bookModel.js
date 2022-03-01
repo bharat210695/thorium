67b0d6469459c3a80cc4f8081344eb13bd82e1d5
https://github.com/bharat210695 <bharat.aggnibachya@gmail.com>
2022-03-01 15:57:21 +0530

    mongo2

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,31 +1,23 @@
 const mongoose = require('mongoose');
 
-const bookSchema = new mongoose.Schema( {
-    bookName: String, 
-    authorName: String, 
+const bookSchema = new mongoose.Schema({
+    bookName: String,
+    authorName: String,
     tags: [String],
-    
+    year: {
+        type: Number,
+        default: 2021
+    },
+    stockAvailable: Boolean,
     isPublished: Boolean,
+    totalPages: Number,
     prices: {
         indianPrice: String,
         europePrice: String,
     },
-    sales: {type: Number, default: 10}
+    sales: { type: Number, default: 10 }
+
 }, { timestamps: true });
 
 
 module.exports = mongoose.model('Book', bookSchema) //users
-
-//Validation:
-//require:true
-//unique
-// default
-
-//String
-//Number
-//Date
-//Boolean
-// Arrays
-// Object
-// ObjectId
-// Buffer - not cover
