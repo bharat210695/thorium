128ec364ecf16162468bac28307a020f2ef945f8
https://github.com/bharat210695 <bharat.aggnibachya@gmail.com>
2022-03-10 10:16:39 +0530

    auth

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,14 @@
-const authenticate = function(req, req, next) {
-    //check the token in request header
-    //validate this token
-
-    next()
-}
+const jwt = require('jsonwebtoken')
 
 
 const authorise = function(req, res, next) {
-    // comapre the logged in user's id and the id in request
-    next()
+    let token = req.headers["x-auth-token"];
+    let decodedToken = jwt.verify(token, "functionup-thorium")
+    let toBeUpdatedUserId = req.params.userId;
+    let loggedInUserId = decodedToken.userId;
+    if (loggedInUserId != toBeUpdatedUserId) return res.send({ status: false, msg: "you are not authorized to perform this task" })
+    else { next() }
 }
+
+
+module.exports.authorise = authorise;
