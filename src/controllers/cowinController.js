3d903f5babd3f418b86ace61961a0d6ed67e8781
https://github.com/bharat210695 <bharat.aggnibachya@gmail.com>
2022-03-13 20:46:26 +0530

    axios

--- src/controllers/cowinController.js
+++ src/controllers/cowinController.js
@@ -1,7 +1,7 @@
 let axios = require("axios")
 
 
-let getStates = async function (req, res) {
+let getStates = async function(req, res) {
 
     try {
         let options = {
@@ -12,15 +12,14 @@ let getStates = async function (req, res) {
         console.log(result)
         let data = result.data
         res.status(200).send({ msg: data, status: true })
-    }
-    catch (err) {
+    } catch (err) {
         console.log(err)
         res.status(500).send({ msg: err.message })
     }
 }
 
 
-let getDistricts = async function (req, res) {
+let getDistricts = async function(req, res) {
     try {
         let id = req.params.stateId
         let options = {
@@ -31,14 +30,13 @@ let getDistricts = async function (req, res) {
         console.log(result)
         let data = result.data
         res.status(200).send({ msg: data, status: true })
-    }
-    catch (err) {
+    } catch (err) {
         console.log(err)
         res.status(500).send({ msg: err.message })
     }
 }
 
-let getByPin = async function (req, res) {
+let getByPin = async function(req, res) {
     try {
         let pin = req.query.pincode
         let date = req.query.date
@@ -50,17 +48,16 @@ let getByPin = async function (req, res) {
         let result = await axios(options)
         console.log(result.data)
         res.status(200).send({ msg: result.data })
-    }
-    catch (err) {
+    } catch (err) {
         console.log(err)
         res.status(500).send({ msg: err.message })
     }
 }
 
-let getOtp = async function (req, res) {
+let getOtp = async function(req, res) {
     try {
         let blahhh = req.body
-        
+
         console.log(`body is : ${blahhh} `)
         var options = {
             method: "post",
@@ -71,15 +68,32 @@ let getOtp = async function (req, res) {
         let result = await axios(options)
         console.log(result.data)
         res.status(200).send({ msg: result.data })
-    }
-    catch (err) {
+    } catch (err) {
         console.log(err)
         res.status(500).send({ msg: err.message })
     }
 }
 
+let districtId = async function(req, res) {
+    try {
+        let dist = req.query.districtid
+        let date = req.query.date
+        console.log('query params are: ${dist} ${date}')
+        var options = {
+            method: "get",
+            url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${dist}&date=${date}`
+        }
+        let result = await axios(options)
+        console.log(result.data)
+        res.status(200).send({ msg: result.data })
+    } catch (err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
 
 module.exports.getStates = getStates
 module.exports.getDistricts = getDistricts
 module.exports.getByPin = getByPin
 module.exports.getOtp = getOtp
+module.exports.districtId = districtId
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const CowinController= require("../controllers/cowinController")
+const CowinController = require("../controllers/cowinController")
+const memesController = require("../controllers/memesController")
+const weatherController = require("../controllers/weatherController")
 
-
-
-router.get("/test-me", function (req, res) {
+router.get("/test-me", function(req, res) {
     res.send("My first ever api!")
 })
 
@@ -12,10 +12,17 @@ router.get("/test-me", function (req, res) {
 router.get("/cowin/states", CowinController.getStates)
 router.get("/cowin/districtsInState/:stateId", CowinController.getDistricts)
 router.get("/cowin/getByPin", CowinController.getByPin)
-
 router.post("/cowin/getOtp", CowinController.getOtp)
+router.get("/cowin/findByDistrictId", CowinController.districtId)
 
-// WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any given district id and for any given date
+router.get("/getCities", weatherController.getCities)
+router.get("/getWeather", weatherController.getWeather)
+router.get("/tempOfLondon", weatherController.tempOfLondon)
+
+router.get("/getMemes", memesController.getMemes)
+router.post("/meme", memesController.memeCreate)
+
+
 
 
 
